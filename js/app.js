6f109ed7b935eddcd5de58d1be112f3f1215fb55
Chris Casey <cdcasey@gmail.com>
2018-03-05 13:36:40 -0600

    resolve #1 basic login

---
+++ js/app.js
@@ -0,0 +1,97 @@
+/*
+fetch('https://api.quizlet.com/2.0/sets/415?client_id=TEaPfm5BsY', {
+    mode: 'no-cors'
+})
+    .then((response) => {
+        return response
+    })
+    .then((data) => {
+        let terms = data.terms;
+        console.log(data);
+
+    })
+*/
+
+(function () {
+    var config = {
+        apiKey: "AIzaSyBiHh1j4ExmaGdaRCWuYov18mOAn58WYJw",
+        authDomain: "flashy-5ded7.firebaseapp.com",
+        databaseURL: "https://flashy-5ded7.firebaseio.com",
+        projectId: "flashy-5ded7",
+        storageBucket: "",
+        messagingSenderId: "89592375367"
+    };
+    firebase.initializeApp(config);
+
+    const auth = firebase.auth();
+    const loginButton = document.getElementById('login');
+    loginButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        const emailInput = getInputValue('email');
+        let passwordInput = getInputValue('password');
+        auth.signInWithEmailAndPassword(emailInput, passwordInput)
+        .catch((error) => console.log(error.message));
+        passwordInput = '';
+    })
+    // auth.createUserWithEmailAndPassword(email, password);
+    const logoutButton = document.getElementById('logout');
+    logoutButton.addEventListener('click', (event) => {
+        auth.signOut();
+    })
+
+    let loginContainer = document.getElementsByClassName('login-container')[0];
+    auth.onAuthStateChanged(user => {
+        if (user) {
+            loginContainer.classList.add('login-container-inactive');
+        } else {
+            console.log("NOPE");
+            loginContainer.classList.remove('login-container-inactive');
+
+        }
+    });
+
+    let objectElement = document.getElementById('object');
+    const uList = document.getElementById('list');
+
+    let database = firebase.database();
+    let dbRefObj = database.ref().child('object');
+    const dbRefList = dbRefObj.child('hobbie');
+
+    dbRefObj.on('value', snap => {
+        objectElement.innerText = JSON.stringify(snap.val(), null, 2);
+    });
+
+    dbRefList.on('child_added', snap => {
+        const li = document.createElement('li');
+        li.innerText = snap.val();
+        li.id = snap.key;
+        uList.appendChild(li);
+    });
+
+    dbRefList.on('child_changed', snap => {
+        const liChanged = document.getElementById(snap.key);
+        liChanged.innerText = snap.val();
+    });
+
+    dbRefList.on('child_removed', snap => {
+        const liToRemove = document.getElementById(snap.key);
+        liToRemove.remove();
+    });
+
+    function getInputValue(elementId) {
+        return document.getElementById(elementId).value;
+    }
+})();
+
+
+/*
+function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('test/' + userId).set({
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
+
+writeUserData('cdc', 'chris', 'jim@jim', '/path/to/image');
+*/
