918fa30eb7075113facfa813582e8207c9065a2a
Chris Casey <cdcasey@gmail.com>
2018-03-06 10:45:18 -0600

    Switching to const for major functions. Also fixed a bug which prevented the DB from being manually edited after logout.

--- js/app.js
+++ js/app.js
@@ -10,24 +10,9 @@ fetch('https://api.quizlet.com/2.0/sets/415?client_id=TEaPfm5BsY', {
         console.log(data);
 
     })
-
-let dbControl = (function () {
-
-})();
-
-let uiControl = (function () {
-
-})();
-
-let control = (function (ui, db) {
-
-})(dbControl, uiControl);
-
-control();
 */
-
-(function () {
-    var config = {
+const dbControl = (function () {
+    let config = {
         apiKey: "AIzaSyBiHh1j4ExmaGdaRCWuYov18mOAn58WYJw",
         authDomain: "flashy-5ded7.firebaseapp.com",
         databaseURL: "https://flashy-5ded7.firebaseio.com",
@@ -39,65 +24,152 @@ control();
 
     let database = firebase.database();
     let deckRef = database.ref().child('decks');
-    // const deckRefList = deckRef.child('hobbie');
-
     const auth = firebase.auth();
-    const loginButton = document.getElementById('login');
-    const logoutButton = document.getElementById('logout');
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
-    const deckContainer = document.getElementById('decks');
-    const quizContainer = document.getElementById('quiz');
-    const deckList = document.getElementById('deck-list');
-    const cardList = document.getElementById('card-list');
 
-    loginButton.addEventListener('click', (event) => {
+    return {
+        authorize: function (email, password, passwordInput = null) {
+            auth.signInWithEmailAndPassword(email, password)
+                .catch((error) => {
+                    console.log(error.message)
+                    passwordInput.value = '';
+                });
+        },
+
+        getAuth: function () {
+            return auth;
+        },
+
+        getDB: function () {
+            return database;
+        },
+
+        closeConnection: function () {
+            database.goOffline();
+        },
+
+        getDeckTemplate: function (deckId) {
+            return deckRef.child(deckId).once('value');;
+        }
+    }
+
+})();
+
+const uiControl = (function () {
+    const DOMelements = {
+        loginContainer: document.getElementsByClassName('login-container')[0],
+        loginButton: document.getElementById('login'),
+        logoutButton: document.getElementById('logout'),
+        emailInput: document.getElementById('email'),
+        passwordInput: document.getElementById('password'),
+        deckContainer: document.getElementById('decks'),
+        quizContainer: document.getElementById('quiz'),
+        deckList: document.getElementById('deck-list'),
+        cardList: document.getElementById('card-list'),
+    }
+
+    return {
+        changeUiMode: function (mode) {
+            switch (mode) {
+                case 'login':
+                    DOMelements.loginContainer.classList.remove('login-container-inactive');
+                    DOMelements.deckContainer.classList.add('decks-inactive');
+                    DOMelements.quizContainer.classList.add('quiz-inactive');
+                    DOMelements.logoutButton.classList.add('logout-button-inactive');
+                    break;
+
+                case 'decks':
+                    DOMelements.loginContainer.classList.add('login-container-inactive');
+                    DOMelements.deckContainer.classList.remove('decks-inactive');
+                    DOMelements.quizContainer.classList.add('quiz-inactive');
+                    DOMelements.logoutButton.classList.remove('logout-button-inactive');
+                    break;
+
+                case 'quiz':
+                    DOMelements.loginContainer.classList.add('login-container-inactive');
+                    DOMelements.deckContainer.classList.add('decks-inactive');
+                    DOMelements.quizContainer.classList.remove('quiz-inactive');
+                    DOMelements.logoutButton.classList.remove('logout-button-inactive');
+                    break;
+
+                default:
+                    break;
+            }
+        },
+
+        getDomElements: function () {
+            return DOMelements;
+        }
+    }
+
+})();
+
+const control = (function (db, ui) {
+    const DOMelements = ui.getDomElements();
+    const auth = db.getAuth();
+    const database = db.getDB();
+
+    let setupEventListeners = function () {
+
+        DOMelements.loginButton.addEventListener('click', login);
+
+        DOMelements.logoutButton.addEventListener('click', (event) => {
+            // Without this call, simply logging out does not release the socket
+            // to the database.
+            db.closeConnection();
+            auth.signOut();
+        })
+
+        DOMelements.deckList.addEventListener('click', (event) => {
+            const deckId = event.target.id
+            const currentUser = auth.currentUser.uid;
+            const userRef = database.ref('/' + currentUser);
+            const clickedDeck = db.getDeckTemplate(deckId);
+            // console.log("DEBUG deck", clickedDeck);
+
+            // TODO: fixed this section
+            userRef.on('value', (snapshot) => {
+                if (snapshot.exists()) {
+                    return false;
+                } else {
+                    clickedDeck.then((snapshot) => {
+                        console.log('DEBUG', snapshot.val());
+                        userRef.set({
+                            [snapshot.key]: snapshot.val(),
+                        })
+                    })
+                }
+            })
+        })
+    }
+
+    function login(event) {
         event.preventDefault();
-        auth.signInWithEmailAndPassword(emailInput.value, passwordInput.value)
-        .catch((error) => {
-            console.log(error.message)
-            passwordInput.value = '';
-        });
-    })
-    // auth.createUserWithEmailAndPassword(email, password);
+        db.authorize(DOMelements.emailInput.value, DOMelements.passwordInput.value, DOMelements.passwordInput);
+    }
 
-    logoutButton.addEventListener('click', (event) => {
-        auth.signOut();
-    })
+    database.ref().child('decks').on('child_added', snapshot => {
+        const li = document.createElement('li');
+        li.innerText = snapshot.key;
+        li.id = snapshot.key;
+        DOMelements.deckList.appendChild(li);
+    });
 
-    let loginContainer = document.getElementsByClassName('login-container')[0];
     auth.onAuthStateChanged(user => {
-        passwordInput.value = '';
+        DOMelements.passwordInput.value = '';
         if (user) {
-            changeUiMode('decks');
+            ui.changeUiMode('decks');
             // loginContainer.classList.add('login-container-inactive');
             // deckContainer.classList.remove('decks-inactive');
         } else {
-            changeUiMode('login');
+            ui.changeUiMode('login');
             // loginContainer.classList.remove('login-container-inactive');
             // deckContainer.classList.add('decks-inactive');
         }
     });
 
-    deckList.addEventListener('click', (event) => {
-        const deckId = event.target.id
-        const currentUser = firebase.auth().currentUser.uid;
-        const userRef = database.ref('/' + currentUser);
-        const clickedDeck = deckRef.child(deckId).once('value');
-        // console.log("DEBUG", clickedDeck.once('value'));
+    /*
 
-        userRef.on('value', (snapshot) => {
-            if (snapshot.exists()) {
-                return false;
-            } else {
-                clickedDeck.then((snapshot) => {
-                    console.log('DEBUG', snapshot.val());
-                    userRef.set({
-                        [snapshot.key]: snapshot.val(),
-                    })
-                })
-            }
-        })
+
 
         startQuiz(userRef, deckId);
         // database.ref('/decks/' + deckId).once('value').then(function(snapshot) {
@@ -112,49 +184,38 @@ control();
         //   });
     })
 
+    function startQuiz(userRef, deckId) {
+        console.log(userRef, deckId);
+        changeUiMode('quiz');
+    }
+*/
+    return {
+        init: function () {
+            console.log("init");
+            setupEventListeners();
+            ui.changeUiMode('login');
+        }
+    }
+})(dbControl, uiControl);
+
+control.init();
+
+
+// (function () {
+
+    // const deckRefList = deckRef.child('hobbie');
+
+
+    // auth.createUserWithEmailAndPassword(email, password);
+
+
     // deckRef.on('value', snap => {
     //     objectElement.innerText = JSON.stringify(snap.val(), null, 2);
     // });
 
-    function startQuiz(userRef, deckId) {
-        console.log(userRef, deckId);
-        changeUiMode('quiz');
 
-    }
 
-    function changeUiMode(mode) {
-        switch (mode) {
-            case 'login':
-                loginContainer.classList.remove('login-container-inactive');
-                deckContainer.classList.add('decks-inactive');
-                quizContainer.classList.add('quiz-inactive');
-                logoutButton.classList.add('logout-button-inactive');
-                break;
 
-            case 'decks':
-                loginContainer.classList.add('login-container-inactive');
-                deckContainer.classList.remove('decks-inactive');
-                quizContainer.classList.add('quiz-inactive');
-                logoutButton.classList.remove('logout-button-inactive');
-                break;
-
-            case 'quiz':
-                loginContainer.classList.add('login-container-inactive');
-                deckContainer.classList.add('decks-inactive');
-                quizContainer.classList.remove('quiz-inactive');
-                logoutButton.classList.remove('logout-button-inactive');
-                break;
-
-            default:
-                break;
-        }
-    }
-    deckRef.on('child_added', snap => {
-        const li = document.createElement('li');
-        li.innerText = snap.key;
-        li.id = snap.key;
-        deckList.appendChild(li);
-    });
 
     // deckRefList.on('child_changed', snap => {
     //     const liChanged = document.getElementById(snap.key);
@@ -166,7 +227,7 @@ control();
     //     liToRemove.remove();
     // });
 
-})();
+// })();
 
 
 /*
