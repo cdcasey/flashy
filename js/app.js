1ddfbbdb6709f070d13ab1c62a08d677f381b892
Chris Casey <cdcasey@gmail.com>
2018-03-05 20:06:00 -0600

    #2 Switching between modes works. Also a safety commit before I start breaking things with a code reorg.

--- js/app.js
+++ js/app.js
@@ -10,6 +10,20 @@ fetch('https://api.quizlet.com/2.0/sets/415?client_id=TEaPfm5BsY', {
         console.log(data);
 
     })
+
+let dbControl = (function () {
+
+})();
+
+let uiControl = (function () {
+
+})();
+
+let control = (function (ui, db) {
+
+})(dbControl, uiControl);
+
+control();
 */
 
 (function () {
@@ -23,10 +37,19 @@ fetch('https://api.quizlet.com/2.0/sets/415?client_id=TEaPfm5BsY', {
     };
     firebase.initializeApp(config);
 
+    let database = firebase.database();
+    let deckRef = database.ref().child('decks');
+    // const deckRefList = deckRef.child('hobbie');
+
     const auth = firebase.auth();
-    let loginButton = document.getElementById('login');
+    const loginButton = document.getElementById('login');
+    const logoutButton = document.getElementById('logout');
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
+    const deckContainer = document.getElementById('decks');
+    const quizContainer = document.getElementById('quiz');
+    const deckList = document.getElementById('deck-list');
+    const cardList = document.getElementById('card-list');
 
     loginButton.addEventListener('click', (event) => {
         event.preventDefault();
@@ -37,7 +60,7 @@ fetch('https://api.quizlet.com/2.0/sets/415?client_id=TEaPfm5BsY', {
         });
     })
     // auth.createUserWithEmailAndPassword(email, password);
-    const logoutButton = document.getElementById('logout');
+
     logoutButton.addEventListener('click', (event) => {
         auth.signOut();
     })
@@ -46,41 +69,102 @@ fetch('https://api.quizlet.com/2.0/sets/415?client_id=TEaPfm5BsY', {
     auth.onAuthStateChanged(user => {
         passwordInput.value = '';
         if (user) {
-            loginContainer.classList.add('login-container-inactive');
+            changeUiMode('decks');
+            // loginContainer.classList.add('login-container-inactive');
+            // deckContainer.classList.remove('decks-inactive');
         } else {
-            console.log("NOPE");
-            loginContainer.classList.remove('login-container-inactive');
-
+            changeUiMode('login');
+            // loginContainer.classList.remove('login-container-inactive');
+            // deckContainer.classList.add('decks-inactive');
         }
     });
 
-    let objectElement = document.getElementById('object');
-    const uList = document.getElementById('list');
+    deckList.addEventListener('click', (event) => {
+        const deckId = event.target.id
+        const currentUser = firebase.auth().currentUser.uid;
+        const userRef = database.ref('/' + currentUser);
+        const clickedDeck = deckRef.child(deckId).once('value');
+        // console.log("DEBUG", clickedDeck.once('value'));
 
-    let database = firebase.database();
-    let dbRefObj = database.ref().child('object');
-    const dbRefList = dbRefObj.child('hobbie');
+        userRef.on('value', (snapshot) => {
+            if (snapshot.exists()) {
+                return false;
+            } else {
+                clickedDeck.then((snapshot) => {
+                    console.log('DEBUG', snapshot.val());
+                    userRef.set({
+                        [snapshot.key]: snapshot.val(),
+                    })
+                })
+            }
+        })
 
-    dbRefObj.on('value', snap => {
-        objectElement.innerText = JSON.stringify(snap.val(), null, 2);
-    });
+        startQuiz(userRef, deckId);
+        // database.ref('/decks/' + deckId).once('value').then(function(snapshot) {
+        //     // console.log(snapshot.val());
+        //     let cards = snapshot.val().cards;
+        //     cards.forEach(card => {
+        //         const li = document.createElement('li');
+        //         li.innerText = card.question;
+        //         cardList.appendChild(li);
+        //     });
 
-    dbRefList.on('child_added', snap => {
+        //   });
+    })
+
+    // deckRef.on('value', snap => {
+    //     objectElement.innerText = JSON.stringify(snap.val(), null, 2);
+    // });
+
+    function startQuiz(userRef, deckId) {
+        console.log(userRef, deckId);
+        changeUiMode('quiz');
+
+    }
+
+    function changeUiMode(mode) {
+        switch (mode) {
+            case 'login':
+                loginContainer.classList.remove('login-container-inactive');
+                deckContainer.classList.add('decks-inactive');
+                quizContainer.classList.add('quiz-inactive');
+                logoutButton.classList.add('logout-button-inactive');
+                break;
+
+            case 'decks':
+                loginContainer.classList.add('login-container-inactive');
+                deckContainer.classList.remove('decks-inactive');
+                quizContainer.classList.add('quiz-inactive');
+                logoutButton.classList.remove('logout-button-inactive');
+                break;
+
+            case 'quiz':
+                loginContainer.classList.add('login-container-inactive');
+                deckContainer.classList.add('decks-inactive');
+                quizContainer.classList.remove('quiz-inactive');
+                logoutButton.classList.remove('logout-button-inactive');
+                break;
+
+            default:
+                break;
+        }
+    }
+    deckRef.on('child_added', snap => {
         const li = document.createElement('li');
-        li.innerText = snap.val();
+        li.innerText = snap.key;
         li.id = snap.key;
-        uList.appendChild(li);
+        deckList.appendChild(li);
     });
 
-    dbRefList.on('child_changed', snap => {
-        const liChanged = document.getElementById(snap.key);
-        liChanged.innerText = snap.val();
-    });
+    // deckRefList.on('child_changed', snap => {
+    //     const liChanged = document.getElementById(snap.key);
+    //     liChanged.innerText = snap.val();
+    // });
 
-    dbRefList.on('child_removed', snap => {
-        const liToRemove = document.getElementById(snap.key);
-        liToRemove.remove();
-    });
+    // deckRefList.on('child_removed', snap => {
+    //     const liToRemove = document.getElementById(snap.key);
+    //     liToRemove.remove();
+    // });
 
 })();
 
